c24b5f8d8978ce91893ed9d787bd44f7d90087d7
Aaron <aaronjeffersonvillanueva15@outlook.com>
2020-08-16 17:36:14 +0800

    added categories, card adjustment added, shirt icon on title

--- src/components/products/ProductItems.vue
+++ src/components/products/ProductItems.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="column is-one-quarter" :key="keyProd">
+  <div class="column is-one-third" :key="keyProd">
     <div class="card">
       <div class="card-image">
         <figure class="image is-4by3">
@@ -9,15 +9,26 @@
       <div class="card-content">
         <div class="media">
           <div class="media-left">
-            <figure class="image is-48x48">
-              <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
-            </figure>
+            <b-icon icon="tshirt-crew" size="is-large" type="is-primary"></b-icon>
           </div>
           <div class="media-content">
-            <p class="title is-4">{{ product.name }}</p>
-            <p class="subtitle is-6">@johnsmith</p>
+            <p class="title is-5">{{ product.name }}</p>
+            <p class="subtitle is-7">@johnsmith</p>
           </div>
         </div>
+
+        <b-field grouped group-multiline>
+          <template v-for="(item, index) in product.categories">
+            <div class="control" :key="index">
+              <b-taglist attached>
+                <b-tag type="is-dark">
+                  <b-icon icon="view-dashboard" size="is-small"></b-icon>
+                </b-tag>
+                <b-tag type="is-info">{{ item.name }}</b-tag>
+              </b-taglist>
+            </div>
+          </template>
+        </b-field>
 
         <div class="content" v-html="product.short_description">
           <br />
